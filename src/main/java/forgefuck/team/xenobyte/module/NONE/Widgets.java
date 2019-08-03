c0e201640554f4d051e283f2d06f768406e23a0b
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2019-08-03 22:02:40 +0300

    crash fix + hide keyabled modules

--- src/main/java/forgefuck/team/xenobyte/module/NONE/Widgets.java
+++ src/main/java/forgefuck/team/xenobyte/module/NONE/Widgets.java
@@ -13,6 +13,7 @@ import forgefuck.team.xenobyte.api.gui.WidgetMessage;
 import forgefuck.team.xenobyte.api.gui.WidgetMode;
 import forgefuck.team.xenobyte.api.module.CheatModule;
 import forgefuck.team.xenobyte.gui.click.elements.GuiWidget;
+import forgefuck.team.xenobyte.module.MISC.Keyabled;
 import forgefuck.team.xenobyte.render.GuiScaler;
 import forgefuck.team.xenobyte.utils.TickHelper;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
@@ -21,6 +22,7 @@ public class Widgets extends CheatModule {
     
     private Map<CheatModule, GuiWidget> keyabled, modulesInfo;
     private List<GuiWidget> infoWidgets;
+    private Keyabled keyabledInfo;
     private int infoWidgetsDelay;
     
     public Widgets() {
@@ -32,6 +34,7 @@ public class Widgets extends CheatModule {
     
     @Override public void onPostInit() {
         keyabledMessage(moduleHandler().xenoGui());
+        keyabledInfo = (Keyabled) moduleHandler().getModuleByClass(Keyabled.class);
         moduleHandler().categoryedModules().filter(CheatModule::provideStateEvents).forEach(this::keyabledMessage);
     }
     
@@ -74,18 +77,22 @@ public class Widgets extends CheatModule {
     }
     
     private void updateInfoPoses() {
-        modulesInfo = sorted(modulesInfo);
-        int height = 0;
-        for (GuiWidget w : modulesInfo.values()) {
-            height += w.getHeight();
-            w.setPos(GuiScaler.scaledScreenWidth() - w.getWidth(), GuiScaler.scaledScreenHeight() - height);
+        if (GuiScaler.isGuiCreated()) {
+            modulesInfo = sorted(modulesInfo);
+            int height = 0;
+            for (GuiWidget w : modulesInfo.values()) {
+                height += w.getHeight();
+                w.setPos(GuiScaler.scaledScreenWidth() - w.getWidth(), GuiScaler.scaledScreenHeight() - height);
+            }
         }
     }
     
     private void updateWidgetPoses() {
-        infoWidgets.forEach(w -> {
-            w.setY(GuiScaler.scaledScreenHeight() - w.getHeight() * (infoWidgets.indexOf(w) + 1));
-        });
+        if (GuiScaler.isGuiCreated()) {
+            infoWidgets.forEach(w -> {
+                w.setY(GuiScaler.scaledScreenHeight() - w.getHeight() * (infoWidgets.indexOf(w) + 1));
+            });
+        }
     }
     
     private LinkedHashMap sorted(Map <CheatModule, GuiWidget> map) {
@@ -104,9 +111,10 @@ public class Widgets extends CheatModule {
     }
     
     @Override public void onDrawGuiOverlay() {
-        Stream.of(modulesInfo.values(), keyabled.values()).forEach(c -> {
-            c.forEach(GuiWidget::draw);
-        });
+        modulesInfo.values().forEach(GuiWidget::draw);
+        if (moduleHandler().isEnabled(keyabledInfo)) {
+            keyabled.values().forEach(GuiWidget::draw);
+        }
     }
     
     @Override public void onTick(boolean inGame) {
