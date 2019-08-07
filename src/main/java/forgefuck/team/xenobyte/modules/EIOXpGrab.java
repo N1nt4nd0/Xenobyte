dc797b736b00a596d1936ce1383e8a94d81d00f4
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2019-08-07 17:35:56 +0300

    new packet module

---
+++ src/main/java/forgefuck/team/xenobyte/modules/EIOXpGrab.java
@@ -0,0 +1,36 @@
+package forgefuck.team.xenobyte.modules;
+
+import cpw.mods.fml.common.Loader;
+import forgefuck.team.xenobyte.api.module.Category;
+import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.api.module.PerformMode;
+import forgefuck.team.xenobyte.api.module.PerformSource;
+import net.minecraft.tileentity.TileEntity;
+
+public class EIOXpGrab extends CheatModule {
+
+    public EIOXpGrab() {
+        super("EIOXpGrab", Category.MODS, PerformMode.SINGLE);
+    }
+    
+    private void sendGrab(TileEntity tile) {
+        try {
+            if (Class.forName("crazypants.enderio.machine.obelisk.xp.TileExperienceObelisk").isInstance(tile)) {
+                utils.sendPacket("enderio", (byte) 69, utils.coords(tile), Short.MAX_VALUE);
+            }
+        } catch(Exception e) {}
+    }
+    
+    @Override public void onPerform(PerformSource src) {
+        utils.nearTiles().forEach(this::sendGrab);
+    }
+    
+    @Override public boolean isWorking() {
+        return Loader.isModLoaded("EnderIO");
+    }
+    
+    @Override public String moduleDesc() {
+        return "Высасывает опыт из всех обелисков опыта в радиусе";
+    }
+
+}
