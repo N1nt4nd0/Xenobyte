00f35501350a88827b64b34c549309d78899b633
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2019-08-07 17:41:14 +0300

    remove log file

--- src/main/java/forgefuck/team/xenobyte/utils/XenoLogger.java
+++ src/main/java/forgefuck/team/xenobyte/utils/XenoLogger.java
@@ -7,13 +7,10 @@ import forgefuck.team.xenobyte.api.Xeno;
 
 public class XenoLogger {
     
-    private static FileProvider fileLog = new FileProvider(Config.getLogFile(), true);
     private static Logger log = LogManager.getLogger(Xeno.mod_name);
     
     public static void info(Object message) {
-        String out = Xeno.utils.nullHelper(message).replaceAll("\n", "");
-        fileLog.writeLine(Xeno.utils.formatTime() + " > " + out + "\n");
-        log.info(out);
+        log.info(Xeno.utils.nullHelper(message).replaceAll("\n", ""));
     }
 
 }
