a07d427255be41305277e1d25dfc37f52e8f79db
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2019-11-25 22:51:19 +0300

    delete pre initialization and ModIDChanger

--- src/main/java/forgefuck/team/xenobyte/XenoByte.java
+++ src/main/java/forgefuck/team/xenobyte/XenoByte.java
@@ -4,12 +4,8 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import forgefuck.team.xenobyte.api.Xeno;
-import forgefuck.team.xenobyte.gui.swing.ModidChanger;
 import forgefuck.team.xenobyte.handlers.ModuleHandler;
-import forgefuck.team.xenobyte.handlers.PacketHandler;
-import net.minecraft.client.Minecraft;
 
 @Mod(modid = Xeno.mod_id, name = Xeno.mod_name, version = Xeno.mod_version)
 
@@ -18,12 +14,6 @@ public class XenoByte {
     @EventHandler public void init(FMLInitializationEvent e) {
         if (e == null) {
             starter(null);
-        }
-    }
-    
-    @EventHandler public void preInit(FMLPreInitializationEvent e) {
-        if (e != null) {
-            new ModidChanger().showFrame(true);
         }
     }
     
