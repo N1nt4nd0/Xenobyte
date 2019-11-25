4b847637db729c2d0c2f83b753b362936df869f1
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2019-11-25 22:48:14 +0300

    new fastbreak module

---
+++ src/main/java/forgefuck/team/xenobyte/modules/FastBreak.java
@@ -0,0 +1,24 @@
+package forgefuck.team.xenobyte.modules;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import forgefuck.team.xenobyte.api.module.Category;
+import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.api.module.PerformMode;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+public class FastBreak extends CheatModule {
+
+    public FastBreak() {
+        super("FastBreak", Category.WORLD, PerformMode.TOGGLE);
+    }
+    
+    @SubscribeEvent public void breakSpeed(PlayerEvent.BreakSpeed e) {
+        utils.sendPacket(new C07PacketPlayerDigging(2, e.x, e.y, e.z, 0));
+    }
+    
+    @Override public String moduleDesc() {
+        return "Быстрое ломание блоков";
+    }
+
+}
