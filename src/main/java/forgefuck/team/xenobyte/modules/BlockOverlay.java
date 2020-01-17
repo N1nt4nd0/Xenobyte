ab736638deeb63f79cff4460b7213a21f180917d
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-01-17 22:41:31 +0300

    new modules by Tenon4ik

---
+++ src/main/java/forgefuck/team/xenobyte/modules/BlockOverlay.java
@@ -0,0 +1,47 @@
+package forgefuck.team.xenobyte.modules;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import forgefuck.team.xenobyte.api.module.Category;
+import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.api.module.PerformMode;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+
+public class BlockOverlay extends CheatModule {
+	
+	public BlockOverlay() {
+        super("BlockOverlay", Category.WORLD, PerformMode.TOGGLE);
+    }
+
+    @SubscribeEvent public void worldRender(RenderWorldLastEvent e) {
+        MovingObjectPosition position = utils.mc().objectMouseOver;
+        if(position != null && utils.world() != null) {
+            Block block = utils.world().getBlock(position.blockX, position.blockY, position.blockZ);
+            if (block != null && block.getMaterial() != Material.air) {
+                utils.mc().entityRenderer.disableLightmap(0.0f);
+                int x = position.blockX, y = position.blockY, z = position.blockZ;
+                render.WORLD.drawOutlinedEspBlock(x, y, z, 0.0f, 0.0f, 0.0f, 0.4F, 1);
+                render.WORLD.drawEspBlock(x, y, z, 0.0f, 0.4f, 0.0f, 0.4F, 1);
+                utils.mc().entityRenderer.enableLightmap(0.0f);
+                return;
+            }
+        }
+        MovingObjectPosition mop = utils.mc().renderViewEntity.rayTrace(200, 1.0F);
+        if(mop != null) {
+            int blockHitSide = mop.sideHit;
+            int x = mop.blockX, y = mop.blockY, z = mop.blockZ;
+            if(utils.world().getBlock(x, y, z).getMaterial() != Material.air) {
+                render.WORLD.drawOutlinedEspBlock(x, y, z, 0.0f, 0.0f, 0.0f, 0.4F, 1);
+                render.WORLD.drawEspBlock(x, y, z, 0.4f, 0.0f, 0.0f, 0.4F, 1);
+                utils.mc().entityRenderer.enableLightmap(0.0f);
+            }
+        }
+    }
+    
+    @Override public String moduleDesc() {
+        return "Показывает можно ли взаимодействовать с блоком";
+    }
+
+}
---
+++ src/main/java/forgefuck/team/xenobyte/modules/Rocket.java
@@ -0,0 +1,40 @@
+package forgefuck.team.xenobyte.modules;
+
+import forgefuck.team.xenobyte.api.config.Cfg;
+import forgefuck.team.xenobyte.api.module.Category;
+import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.api.module.PerformMode;
+import forgefuck.team.xenobyte.api.module.PerformSource;
+import forgefuck.team.xenobyte.gui.click.elements.Panel;
+import forgefuck.team.xenobyte.gui.click.elements.ScrollSlider;
+
+public class Rocket extends CheatModule {
+	
+	@Cfg("strength") private int strength;
+    public Rocket() {
+        super("Rocket", Category.MOVE, PerformMode.SINGLE);
+        strength = 2;
+    }
+    
+    @Override public void onPerform(PerformSource src) {
+        this.utils.player().addVelocity(0.0d, strength * 0.5d, 0.0d);
+    }
+    
+    @Override public String moduleDesc() {
+        return "Ускорение в небо";
+    }
+    
+    @Override public Panel settingPanel() {
+        return new Panel(
+                new ScrollSlider("Strength", strength, 30) {
+                    @Override public void onScroll(int dir, boolean withShift) {
+                        strength = processSlider(dir, withShift);
+                    }
+                    @Override public String elementDesc() {
+                        return "Величина ускорения";
+                    }
+                }
+        );
+    }
+
+}
