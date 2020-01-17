006e52eb584c626abe55bdbc948fc756bb91d39b
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-01-17 22:43:50 +0300

    test vanished nicknames

--- src/main/java/forgefuck/team/xenobyte/modules/CheckVanish.java
+++ src/main/java/forgefuck/team/xenobyte/modules/CheckVanish.java
@@ -1,13 +1,20 @@
 package forgefuck.team.xenobyte.modules;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.google.gson.JsonObject;
+
+import forgefuck.team.xenobyte.api.config.Cfg;
 import forgefuck.team.xenobyte.api.gui.WidgetMessage;
 import forgefuck.team.xenobyte.api.gui.WidgetMode;
 import forgefuck.team.xenobyte.api.module.Category;
 import forgefuck.team.xenobyte.api.module.CheatModule;
 import forgefuck.team.xenobyte.api.module.PerformMode;
-import forgefuck.team.xenobyte.utils.TickHelper;
+import forgefuck.team.xenobyte.gui.click.elements.Button;
+import forgefuck.team.xenobyte.gui.click.elements.Panel;
+import forgefuck.team.xenobyte.utils.NetUtils;
 import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.client.multiplayer.ServerAddress;
 import net.minecraft.client.multiplayer.ServerData;
@@ -23,19 +30,23 @@ import net.minecraft.util.IChatComponent;
 
 public class CheckVanish extends CheatModule {
     
-    private INetHandlerStatusClient vanishHandler;
+    private INetHandlerStatusClient simpleHandler;
+    @Cfg("withNames") private boolean withNames;
+    private List<String> vanishNames;
     private WidgetMessage outMessage;
+    private boolean doMcsrv;
     
     public CheckVanish() {
         super("CheckVanish", Category.MISC, PerformMode.TOGGLE);
         outMessage = new WidgetMessage(this, "...", WidgetMode.INFO);
-        vanishHandler = new INetHandlerStatusClient() {
+        vanishNames = new ArrayList<String>();
+        simpleHandler = new INetHandlerStatusClient() {
             @Override public void handleServerInfo(S00PacketServerInfo infoPacket) {
                 ServerStatusResponse response = infoPacket.func_149294_c();
                 if (response.func_151318_b() != null) {
-                    int vCount = response.func_151318_b().func_151333_b() - utils.player().sendQueue.playerInfoList.size();
-                    boolean isVanish = vCount > 0;
-                    outMessage = new WidgetMessage(CheckVanish.this, String.valueOf(isVanish ? vCount : 0), isVanish ? WidgetMode.FAIL : WidgetMode.SUCCESS);
+                	int vCount = response.func_151318_b().func_151333_b() - utils.tabList().size();
+                	int count = vCount > 0 ? vCount : 0;
+                	outMessage = new WidgetMessage(CheckVanish.this, count + (withNames ? " " + vanishNames : ""), count > 0 ? WidgetMode.FAIL : WidgetMode.SUCCESS);
                 }
             }
             @Override public void onConnectionStateTransition(EnumConnectionState e, EnumConnectionState e1) {}
@@ -43,37 +54,79 @@ public class CheckVanish extends CheatModule {
             @Override public void handlePong(S01PacketPong var) {}
             @Override public void onNetworkTick() {}
         };
+        withNames = true;
+        doMcsrv = true;
+    }
+
+	private void simpleRequest(String ip) {
+        try {
+            ServerAddress addr = ServerAddress.func_78860_a(ip);
+            NetworkManager manager = NetworkManager.provideLanClient(InetAddress.getByName(addr.getIP()), addr.getPort());
+            manager.setNetHandler(simpleHandler);
+            manager.scheduleOutboundPacket(new C00Handshake(5, addr.getIP(), addr.getPort(), EnumConnectionState.STATUS), new GenericFutureListener[0]);
+            manager.scheduleOutboundPacket(new C00PacketServerQuery(), new GenericFutureListener[0]);
+        } catch (Exception ex) {}
     }
     
-    private void sendCheckRequest(ServerData data) {
-        if (data != null) {
-            try {
-                ServerAddress addr = ServerAddress.func_78860_a(data.serverIP);
-                NetworkManager manager = NetworkManager.provideLanClient(InetAddress.getByName(addr.getIP()), addr.getPort());
-                manager.setNetHandler(vanishHandler);
-                manager.scheduleOutboundPacket(new C00Handshake(5, addr.getIP(), addr.getPort(), EnumConnectionState.STATUS), new GenericFutureListener[0]);
-                manager.scheduleOutboundPacket(new C00PacketServerQuery(), new GenericFutureListener[0]);
-            } catch (Exception ex) {}
-        } else {
-            outMessage = new WidgetMessage(this, "SP", WidgetMode.INFO);
-        }
+    private List<String> mcsrvRequest(String ip) {
+    	List<String> pls = new ArrayList<String>();
+    	JsonObject check = NetUtils.getServerInfo(ip);
+    	if (check != null && check.has("players")) {
+    		JsonObject players = check.getAsJsonObject("players");
+    		if (players != null && players.has("list")) {
+    			players.getAsJsonArray("list").forEach(p -> {
+        			pls.add(p.getAsString());
+        		});
+    		}
+    	}
+    	return pls;
     }
     
-    @Override public int tickDelay() {
-        return TickHelper.THREE_SEC;
+    @Override public void onPostInit() {
+        new Thread(() -> {
+        	while (true) {
+        		if (utils.isInGame()) {
+                	ServerData data = utils.mc().func_147104_D();
+                	if (data != null) {
+                		simpleRequest(data.serverIP);
+                		if (doMcsrv && withNames) {
+                			List<String> check = mcsrvRequest(data.serverIP);
+                			doMcsrv = !check.isEmpty();
+                			vanishNames.clear();
+                			check.stream().filter(n -> !utils.tabList().contains(n)).forEach(vanishNames::add);
+                		}
+                	} else {
+                		outMessage = new WidgetMessage(this, "SP", WidgetMode.INFO);
+                	}
+        		}
+            	try {
+            		Thread.sleep(5000);
+            	} catch(Exception e) {}
+        	}
+        }).start();
     }
     
     @Override public void onTick(boolean inGame) {
         if (inGame) {
-            new Thread(() -> sendCheckRequest(utils.mc().func_147104_D())).start();
-            if (outMessage != null) {
-                moduleHandler().widgets().infoMessage(outMessage);
-            }
+        	moduleHandler().widgets().infoMessage(outMessage);
         }
     }
     
     @Override public String moduleDesc() {
         return "Выводит на инфопанель количество игроков в ванише";
+    }
+    
+    @Override public Panel settingPanel() {
+        return new Panel(
+            new Button("WithNames", withNames) {
+                @Override public void onLeftClick() {
+                    buttonValue(withNames = !withNames);
+                }
+                @Override public String elementDesc() {
+                    return "По возможности выводить имена (beta)";
+                }
+            }
+        );
     }
 
 }
