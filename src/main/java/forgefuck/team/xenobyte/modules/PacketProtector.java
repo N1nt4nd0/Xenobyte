6d86d3ad719480c619248e3fa5a40a5c476f771d
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-05-03 21:47:22 +0300

    new modules

---
+++ src/main/java/forgefuck/team/xenobyte/modules/PacketProtector.java
@@ -0,0 +1,48 @@
+package forgefuck.team.xenobyte.modules;
+
+import cpw.mods.fml.common.network.internal.FMLProxyPacket;
+import forgefuck.team.xenobyte.api.module.Category;
+import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.api.module.PerformMode;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C01PacketChatMessage;
+
+public class PacketProtector extends CheatModule {
+    
+    private EIOTeleport teleporter;
+    private RadioHack radiohack;
+    
+    public PacketProtector() {
+        super("PacketProtector", Category.NONE, PerformMode.ON_START);
+    }
+    
+    @Override public void onPostInit() {
+        teleporter = (EIOTeleport) moduleHandler().getModuleByClass(EIOTeleport.class);
+        radiohack = (RadioHack) moduleHandler().getModuleByClass(RadioHack.class);
+    }
+    
+    @Override public boolean doSendPacket(Packet packet) {
+        if (teleporter.getIntercept()) {
+            if (packet instanceof C01PacketChatMessage) {
+                String[] args = ((C01PacketChatMessage) packet).func_149439_c().replaceAll(" +", " ").split(" ");
+                if ("/tp".equals(args[0]) && args.length == 5) {
+                    teleporter.doTeleport(-1, Integer.valueOf(args[2]), Integer.valueOf(args[3]), Integer.valueOf(args[4]));
+                    return false;
+                } 
+            }
+        } 
+        return true;
+    }
+    
+    @Override public boolean doReceivePacket(Packet packet) {
+        if (radiohack.getKick()) {
+            if (packet instanceof FMLProxyPacket) {
+                if ("DragonsRadioMod".equals(((FMLProxyPacket) packet).channel())) {
+                    return false;
+                }
+            }
+        } 
+        return true;
+    }
+
+}
---
+++ src/main/java/forgefuck/team/xenobyte/modules/ShearDupe.java
@@ -0,0 +1,35 @@
+package forgefuck.team.xenobyte.modules;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import forgefuck.team.xenobyte.api.module.Category;
+import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.api.module.PerformMode;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemShears;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.C02PacketUseEntity;
+import net.minecraftforge.client.event.MouseEvent;
+
+public class ShearDupe extends CheatModule {
+    
+    public ShearDupe() {
+        super("ShearDupe", Category.MISC, PerformMode.TOGGLE);
+    }
+    
+    @SubscribeEvent public void mouseEvent(MouseEvent e) {
+        if (e.button == 1 && e.buttonstate) {
+            Entity ent = utils.entity();
+            ItemStack handItem = utils.item();
+            if (ent != null && !utils.isInCreative() && handItem != null && handItem.getItem() instanceof ItemShears) {
+                for (int i = 0; i < 1000; i++) {
+                    utils.sendPacket(new C02PacketUseEntity(ent, C02PacketUseEntity.Action.INTERACT));
+                }
+            }
+        }
+    }
+    
+    @Override public String moduleDesc() {
+        return "Дюп коров и грибов (ПКМ ножницами по грибной корове)";
+    }
+
+}
