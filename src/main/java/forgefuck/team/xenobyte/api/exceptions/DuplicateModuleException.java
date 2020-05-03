0f0ce3368fd1bd58d482581f38f6b18488852ea5
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-05-04 00:52:22 +0300

    Revert "test"
    This reverts commit 3d045b560eaf2bf0904fe734e53ffd23f511d5e1.

--- src/main/java/forgefuck/team/xenobyte/api/exceptions/DuplicateModuleException.java
+++ src/main/java/forgefuck/team/xenobyte/api/exceptions/DuplicateModuleException.java
@@ -1,6 +1,7 @@
 package forgefuck.team.xenobyte.api.exceptions;
 
 import forgefuck.team.xenobyte.api.module.CheatModule;
+import forgefuck.team.xenobyte.utils.XenoLogger;
 
 public class DuplicateModuleException extends RuntimeException {
     
