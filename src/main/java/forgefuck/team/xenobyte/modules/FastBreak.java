9717f4a9046b243b5a373a92fc3cc5ced8d03d3d
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-05-03 21:50:51 +0300

    better fastbreak

--- src/main/java/forgefuck/team/xenobyte/modules/FastBreak.java
+++ src/main/java/forgefuck/team/xenobyte/modules/FastBreak.java
@@ -1,20 +1,31 @@
 package forgefuck.team.xenobyte.modules;
 
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import forgefuck.team.xenobyte.api.module.Category;
 import forgefuck.team.xenobyte.api.module.CheatModule;
 import forgefuck.team.xenobyte.api.module.PerformMode;
-import net.minecraft.network.play.client.C07PacketPlayerDigging;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import forgefuck.team.xenobyte.utils.Reflections;
+import net.minecraft.block.Block;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.util.MovingObjectPosition.MovingObjectType;
 
 public class FastBreak extends CheatModule {
 
     public FastBreak() {
         super("FastBreak", Category.WORLD, PerformMode.TOGGLE);
     }
-    
-    @SubscribeEvent public void breakSpeed(PlayerEvent.BreakSpeed e) {
-        utils.sendPacket(new C07PacketPlayerDigging(2, e.x, e.y, e.z, 0));
+
+    @Override public void onTick(boolean isInGame) {
+        if (isInGame && utils.mc().gameSettings.keyBindAttack.getIsKeyPressed() && utils.mc().objectMouseOver != null && utils.mc().objectMouseOver.typeOfHit == MovingObjectType.BLOCK) {
+            Reflections.setPrivateValue(PlayerControllerMP.class, utils.mc().playerController, 0, 8);
+            int x = utils.mc().objectMouseOver.blockX;
+            int y = utils.mc().objectMouseOver.blockY;
+            int z = utils.mc().objectMouseOver.blockZ;
+            Block block = utils.block(x, y, z);
+            if (block != null) {
+                float hardness = ((float) Reflections.getPrivateValue(PlayerControllerMP.class, utils.mc().playerController, 6)) + block.getPlayerRelativeBlockHardness(utils.player(), utils.world(), x, y, z);
+                Reflections.setPrivateValue(PlayerControllerMP.class, utils.mc().playerController, hardness, 6);
+            }
+        }
     }
     
     @Override public String moduleDesc() {
