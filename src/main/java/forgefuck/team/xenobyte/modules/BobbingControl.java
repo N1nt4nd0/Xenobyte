f7385c266268839610406070b5d3f0bb498c1a99
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-06-21 11:48:01 +0300

    big bobbing costil

--- src/main/java/forgefuck/team/xenobyte/modules/BobbingControl.java
+++ src/main/java/forgefuck/team/xenobyte/modules/BobbingControl.java
@@ -9,6 +9,7 @@ import net.minecraftforge.client.event.RenderWorldLastEvent;
 
 public class BobbingControl extends CheatModule {
     
+    private PortalHelper phelper;
     private XRay xray;
     private Esp esp;
 
@@ -16,18 +17,24 @@ public class BobbingControl extends CheatModule {
         super("BobbingControl", Category.NONE, PerformMode.ENABLED_ON_START);
     }
     
+    @Override public boolean isWidgetable() {
+        return false;
+    }
+    
     @Override public void onPostInit() {
+        phelper = (PortalHelper) moduleHandler().getModuleByClass(PortalHelper.class);
         xray = (XRay) moduleHandler().getModuleByClass(XRay.class);
         esp = (Esp) moduleHandler().getModuleByClass(Esp.class);
     }
     
-    @SubscribeEvent(priority = EventPriority.HIGHEST) public void renderWorldH(RenderWorldLastEvent e) {
+    @SubscribeEvent(priority = EventPriority.HIGHEST) public void renderWorld(RenderWorldLastEvent e) {
+        phelper.linesCheck = false;
         xray.linesCheck = false;
         esp.linesCheck = false;
     }
     
     @SubscribeEvent(priority = EventPriority.LOWEST) public void renderWorldL(RenderWorldLastEvent e) {
-        utils.mc().gameSettings.viewBobbing = xray.linesCheck && xray.bindLines ? false : !(esp.linesCheck && esp.bindLines);
+        utils.mc().gameSettings.viewBobbing = (!xray.linesCheck || !xray.bindLines) && (!esp.linesCheck || !esp.bindLines) && !phelper.linesCheck;
     }
 
 }
