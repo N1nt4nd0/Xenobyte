c4b95139178f705548422e2c9b05dc96bc0fea22
N1nt4nd0 <kunilingusmagistrus@gmail.com>
2020-09-06 02:46:27 +0300

    Version up and more links

--- src/main/java/forgefuck/team/xenobyte/api/Xeno.java
+++ src/main/java/forgefuck/team/xenobyte/api/Xeno.java
@@ -4,18 +4,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import forgefuck.team.xenobyte.render.Renderer;
+import forgefuck.team.xenobyte.utils.LangProvider;
 import forgefuck.team.xenobyte.utils.Utils;
 
 public interface Xeno {
 
     String mod_id = "xenobyte";
-    String author = "N1nt3nd0";
     String mod_name = "X3N0BYT3";
-    String mod_version = "1.0.8";
+    String mod_version = "1.0.9";
+    String mod_author = "N1nt4nd0";
     String format_prefix = "§8[§4" + mod_name + "§8]§r ";
+    
+    String ds_link = "N1nt4nd0#0613";
+    String tg_link = "t.me/N1nt4nd0";
+    String gh_link = "github.com/N1nt4nd0/Xenobyte";
+    String yt_link = "youtube.com/channel/UClXGh0w1BiBEyxn7iFI4dsA";
 
-    Logger logger = LogManager.getLogger(mod_name);
-    Renderer render = new Renderer();
     Utils utils = new Utils();
+    Renderer render = new Renderer();
+    LangProvider lang = new LangProvider();
+    Logger logger = LogManager.getLogger(mod_name);
     
 }
